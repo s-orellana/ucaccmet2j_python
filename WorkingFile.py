eb78bb2ac66e54165563729cdf28d3e9e0de99fb
s-orellana <sofia.ore.94@gmail.com>
2018-01-08 18:55:48 +0100

    Part 3 - Assignment 3
    Partially submitted part 3

--- WorkingFile.py
+++ WorkingFile.py
@@ -5,81 +5,62 @@ Created on Mon Jan  8 12:44:04 2018
 
 @author: sorellana
 """
-
-'''
-# ----------------------------PART 1----------------------------
-
-'''
-
-#Importing JSON data
 import json
+import csv
 
+#Bring in data
 with open('precipitation.json') as Data: 
     Precipitation_Data = json.load(Data) 
 
-#Define Seattle Station Code:
-SeattleStation = 'GHCND:US1WAKG0038'
+with open('stations.csv') as csvfile:
+    StationData = csv.reader(csvfile, delimiter=',')
+    Station_Location = []
+    Station_Code =[]
+    for row  in  StationData: #Makings sperate lists for station name & code
+        Station_Location.append(row[0])
+        Station_Code.append(row[2])
+        
+print(Station_Location)
+print(Station_Code)
 
-# --- Selecting Seattle measurements
+MetaList={}
 
-SeattleData= []
-for item in Precipitation_Data[0:]:
-   if item['station'] == SeattleStation: #Select measurements
-     SeattleData.append(item) #store them in a llist
-   #print(SeattleData) #Visual check
+#Attaching Location name to the precipitation data - Allows selection. 
+#Creates four groups of data (one per city)
+for item, code in zip(Station_Location[1:], Station_Code[1:]):
+    MetaList[item] = []
+    for point in Precipitation_Data[0:]:
+        if point['station'] == code:
+            MetaList[item].append(point)
+    
+#print(MetaList)
 
 
-''' 
---- Small scale version of the exercise - Summing PRP for january
 
-sum_jan=[] 
-for item in SeattleData[0:]:
-    if item['date'][-5:-3] == '01':
-        sum_jan.append(item['value'])
-sum_jan = sum(sum_jan)
-#print(sum_jan)
-       
-'''
-
- #---- Summing PRP for PER MONTH 
+#--- Getting precipitation 
 list_ofMonths = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11','12']
-PPM= [] # list to store month & total preicipitation as key-value pairs in
-#PPM = Precipitation per month
+PPM = []
+Daily_values= []
 
-for month in list_ofMonths:
-    Temporary_List =[]
-    for item in SeattleData[0:]: #loop through seattle data
-        if item['date'][-5:-3] == month: 
-            Temporary_List.append(item['value']) #Store
-    PPM.append(sum(Temporary_List))  #sum daily values, add them to list
-   # Precip_PMonth.append({'%s' %(month): sum(Temporary_List)})  #Alternative version
+for name in Station_Location[1:]:
+    Temp_City = MetaList[name] #subsets data for the city in current loop
+    for month in list_ofMonths: # run through a month
+        for datapoint in Temp_City:
+            if datapoint['date'][-5:-3] == month:  # does your data match?
+                Daily_values.append(datapoint['value'])
+                
+# From here on: I wanted to create a list of rain per month, per city (as in a list two levels deep: city - 
+# and the a list of per month measurements.)
+            
+    
+print(Daily_values)
+   
+   
+   
 
-#print(PPM) # see output 
-
-#---- Saving to a JSON file
-
-'''
-import json
-with open('PART1_PPM.json', 'w') as JsonFile:
-    json.dump(PPM, JsonFile)
-'''
-        
-# ----------------------------PART 2----------------------------
-
-# Precipitation over the year
-PRCP_Year = sum(PPM)
-print(PRCP_Year)
-
-#Relative precipitation per mont
-
-RelativeP= []
-
-for item in PPM: #Go through precipitation per month 
-  RelativeP.append(item /PRCP_Year) #obtain and store relative prescipitation
-print(RelativeP)
 
         
-        
+       
         
         
         
